36fe0f78057627924bf987693bc482adfc4194c6
Trevin Tejero <trevin808@gmail.com>
2016-10-05 23:12:03 -1000

    Created dynamic port

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
+const PORT = app.get('port')
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile("index.html");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, function () {
+  console.log('Your server is up and running on port:' + PORT);
 });
