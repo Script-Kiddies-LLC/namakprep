496ffeb44c8fc622a11500f4881bb89f8f5885f3
Trevin Tejero <trevin808@gmail.com>
2016-10-05 23:17:21 -1000

    Created dynamic port

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-const PORT = app.get('port')
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
