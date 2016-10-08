b84fdd8219f074bbbb03de366af9b60e647a3f8d
Trevin Tejero <trevin808@gmail.com>
2016-10-08 00:29:27 -1000

    Added mySql & bookshelf.js, /login, and /register

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ app.get('/contact', (req, res) => {
   res.sendFile(__dirname + '/public/contact.html');
 });
 
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(__dirname + '/public/register.html');
+});
+
 app.get('/user', (req, res) => {
   res.sendFile(__dirname + '/public/profile.html');
 });
